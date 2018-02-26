2bd5b7f1177a1ccf5c55612a80e6d936a495e1b5
rmitula <rmitula@gmail.com>
2018-02-26 20:12:01 +0100

    UserControllerTest update

--- src/test/java/pl/rmitula/restfullshop/controller/UserControllerTest.java
+++ src/test/java/pl/rmitula/restfullshop/controller/UserControllerTest.java
@@ -4,60 +4,110 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import pl.rmitula.restfullshop.model.User;
-import pl.rmitula.restfullshop.service.UserService;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-import java.util.List;
+import javax.transaction.Transactional;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@Transactional
 public class UserControllerTest {
 
     private MockMvc mockMvc;
 
-    @Mock
-    private UserService userService;
-
-    @InjectMocks
-    private UserController userController;
+    @Autowired
+    private WebApplicationContext wac;
 
     @Before
     public void setUp() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(userController)
-                .build();
+        mockMvc = webAppContextSetup(this.wac).build();
     }
 
     @Test
     public void GetAllUsersTest() throws Exception {
-        List<User> users = Arrays.asList(
-                new User("John", "Snow", "snow", "johnsnow@blackbastard.com", "password"),
-                new User("Tyrion", "Lanister", "tyrion", "tyrion@lanister.pl", "password"));
-
-        when(userService.getUsers()).thenReturn(users);
-
         mockMvc.perform(get("/api/users"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.[0].firstName", Matchers.is("John")))
-                .andExpect(jsonPath("$.[0].lastName", Matchers.is("Snow")))
-                .andExpect(jsonPath("$.[0].username", Matchers.is("snow")))
-                .andExpect(jsonPath("$.[0].email", Matchers.is("johnsnow@blackbastard.com")))
-                .andExpect(jsonPath("$.[1].firstName", Matchers.is("Tyrion")))
-                .andExpect(jsonPath("$.[1].lastName", Matchers.is("Lanister")))
-                .andExpect(jsonPath("$.[1].username", Matchers.is("tyrion")))
-                .andExpect(jsonPath("$.[1].email", Matchers.is("tyrion@lanister.pl")))
-                .andExpect(jsonPath("$.*", Matchers.hasSize(2)));
+                .andExpect(jsonPath("$.[0].firstName", Matchers.is("Andrzej")))
+                .andExpect(jsonPath("$.[0].lastName", Matchers.is("Duda")))
+                .andExpect(jsonPath("$.[0].username", Matchers.is("admin")))
+                .andExpect(jsonPath("$.[0].email", Matchers.is("aduda@gmail.com")))
+                .andExpect(jsonPath("$.[1].firstName", Matchers.is("Rafal")))
+                .andExpect(jsonPath("$.[1].lastName", Matchers.is("Mitula")))
+                .andExpect(jsonPath("$.[1].username", Matchers.is("rmitula")))
+                .andExpect(jsonPath("$.[1].email", Matchers.is("rmitula@gmail.com")))
+                .andExpect(jsonPath("$.*", Matchers.hasSize(3)));
     }
+
+    @Test
+    public void GetUserByIdTest() throws Exception {
+        mockMvc.perform(get("/api/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.firstName", Matchers.is("Andrzej")))
+                .andExpect(jsonPath("$.lastName", Matchers.is("Duda")))
+                .andExpect(jsonPath("$.username", Matchers.is("admin")))
+                .andExpect(jsonPath("$.email", Matchers.is("aduda@gmail.com")));
+    }
+
+    @Test
+    public void GetUserByIdWithUnknownIdTest() throws Exception {
+        mockMvc.perform(get("/api/users/100"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code", Matchers.is(404)))
+                .andExpect(jsonPath("$.description", Matchers.is("Not found user with id: 100")));
+    }
+
+    @Test
+    public void GetUserByUserNameTest() throws Exception {
+        mockMvc.perform(get("/api/users/findByUserName/admin"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.firstName", Matchers.is("Andrzej")))
+                .andExpect(jsonPath("$.lastName", Matchers.is("Duda")))
+                .andExpect(jsonPath("$.username", Matchers.is("admin")))
+                .andExpect(jsonPath("$.email", Matchers.is("aduda@gmail.com")));
+    }
+
+    @Test
+    public void GetUserByUserNameWithIgnoreCaseTest() throws Exception {
+        mockMvc.perform(get("/api/users/findByUserName/aDMiN"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.firstName", Matchers.is("Andrzej")))
+                .andExpect(jsonPath("$.lastName", Matchers.is("Duda")))
+                .andExpect(jsonPath("$.username", Matchers.is("admin")))
+                .andExpect(jsonPath("$.email", Matchers.is("aduda@gmail.com")));
+    }
+
+    @Test
+    public void GetUserByUnknownUserNameTest() throws Exception {
+        mockMvc.perform(get("/api/users/findByUserName/unknown"))
+                .andExpect(jsonPath("$.code", Matchers.is(404)))
+                .andExpect(jsonPath("$.description", Matchers.is("Not found user with username: unknown")));
+    }
+
+    /* ARCHIVE
+    "...So MockMvc tests simply aren't enough to test error responses generated through Spring Boot...."
+    https://github.com/spring-projects/spring-boot/issues/7321
+
+    @Test
+    public void GetUserByIdWithUnknownIdTest() throws Exception {
+        when(userService.findById(1)).thenThrow(new NotFoundException("Not found user with id: 1"));
+
+        mockMvc.perform(get("/api/users/1"))
+                .andExpect(status().isBadRequest());
+    }
+    */
 
     @Test
     public void CreateUserTest() throws Exception {
@@ -68,19 +118,90 @@ public class UserControllerTest {
     }
 
     @Test
-    public void UpdateUserDetailsTest() throws Exception {
-
-        mockMvc.perform(put("/api/users/{id}", 1)
+    public void CreateUserWithExistingUserNameTest() throws Exception {
+        mockMvc.perform(post("/api/users")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(createUserInJson("John", "Lanister", "tyrion", "tyrion@lanister.pl", "password")))
+                .content(createUserInJson("Adam", "Kowalski", "admin", "adamkowalski@gmail.com", "password")))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.code", Matchers.is(409)))
+                .andExpect(jsonPath("$.description", Matchers.is("This username or email is already associated with a different user account.")));
+    }
+
+    @Test
+    public void CreateUserWithExistingEmailTest() throws Exception {
+        mockMvc.perform(post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("Adam", "Kowalski", "adam", "aduda@gmail.com", "password")))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.code", Matchers.is(409)))
+                .andExpect(jsonPath("$.description", Matchers.is("This username or email is already associated with a different user account.")));
+    }
+
+    @Test
+    public void CreateUserWithoutOneFieldlTest() throws Exception {
+        mockMvc.perform(post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                //Same as createUserInJson but without LastName field.
+                .content("{ \"firstName\": \"Adam\", " +
+                        "\"email\":\"akowalski@gmail.com\"," +
+                        "\"username\":\"akowalski\"," +
+                        "\"password\":\"password\"}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code", Matchers.is(400)));
+    }
+
+    @Test
+    public void UpdateUserDetailsTest() throws Exception {
+        mockMvc.perform(put("/api/users/{id}", 3)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("John", "Lanister", "tyrfion", "tyfrion@lanister.pl", "password")))
                 .andExpect(status().isOk());
     }
 
     @Test
+    public void UpdateUserNameWithAnotherExistingUserNameTest() throws Exception {
+        mockMvc.perform(put("/api/users/{id}", 3)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("John", "Lanister", "admin", "tyfrion@lanister.pl", "password")))
+                .andExpect(status().isConflict())
+                        .andExpect(jsonPath("$.code", Matchers.is(409)))
+                        .andExpect(jsonPath("$.description", Matchers.is("This username is already associated with another user account.")));
+    }
+
+    @Test
+    public void UpdateUserEmailWithAnotherExistingEmailTest() throws Exception {
+        mockMvc.perform(put("/api/users/{id}", 3)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("John", "Lanister", "another", "aduda@gmail.com", "password")))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.code", Matchers.is(409)))
+                .andExpect(jsonPath("$.description", Matchers.is("This email is already associated with another user account.")));
+    }
+
+    @Test
+    public void UpdateUserWithUnknownIdTest() throws Exception {
+        mockMvc.perform(put("/api/users/{id}", 100)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("John", "Lanister", "another", "aduda@gmail.com", "password")))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code", Matchers.is(404)))
+                .andExpect(jsonPath("$.description", Matchers.is("Not found user with id: 100")));
+    }
+
+    @Test
     public void DeleteUserTest() throws Exception {
-        mockMvc.perform(delete("/api/users/{id}", 1)
+        mockMvc.perform(delete("/api/users/{id}", 3)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void DeleteUserWithUnknownIdTest() throws Exception {
+        mockMvc.perform(delete("/api/users/{id}", 100)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code", Matchers.is(404)))
+                .andExpect(jsonPath("$.description", Matchers.is("Not found user with id: 100")));
     }
 
     private static String createUserInJson (String firstName, String lastName, String userName, String email, String password) {
